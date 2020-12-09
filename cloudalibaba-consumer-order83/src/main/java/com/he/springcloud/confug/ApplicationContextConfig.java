553d3b2390a0a89c297549c65e1467d003a1ec8b
992384727 <992384727@qq.com>
2020-12-09 12:00:05 +0800

    Nacos服务支持和配置中心

---
+++ cloudalibaba-consumer-order83/src/main/java/com/he/springcloud/confug/ApplicationContextConfig.java
@@ -0,0 +1,18 @@
+package com.he.springcloud.confug;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class ApplicationContextConfig {
+
+    @Bean
+//    开启负载均衡
+    @LoadBalanced
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }
+
+}
---
+++ cloudalibaba-provider-payment9001/src/main/java/com/he/springcloud/CloudalibabaProviderPayment9001Application.java
@@ -0,0 +1,13 @@
+package com.he.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudalibabaProviderPayment9001Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudalibabaProviderPayment9001Application.class, args);
+    }
+
+}
